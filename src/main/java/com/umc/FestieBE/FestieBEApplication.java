1ceec66c05441338024ced5794f97a36d26bf432
eujjong18 <eujjong18@naver.com>
2023-07-08 14:14:32 +0900

    프로젝트 세팅

---
+++ src/main/java/com/umc/FestieBE/FestieBEApplication.java
@@ -0,0 +1,13 @@
+package com.umc.FestieBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestieBEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestieBEApplication.class, args);
+	}
+
+}
