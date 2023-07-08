becd7c251d36add1b32688194a38dbe8cb1e1d4a
eujjong18 <eujjong18@naver.com>
2023-07-09 02:09:29 +0900

    Nginx를 이용한 무중단 배포 적용 - 배포 스크립트 run_new_was.sh, health_check.sh, switch.sh 생성

---
+++ scripts/run_new_was.sh
@@ -0,0 +1,34 @@
+# run_new_was.sh
+
+#!/bin/bash
+
+# 환경변수 설정 파일 로드
+source ~/.bash_profile
+
+CURRENT_PORT=$(cat /home/ubuntu/service_url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Current port of running WAS is ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+  TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+  TARGET_PORT=8081
+else
+  echo "> No WAS is connected to nginx"
+fi
+
+TARGET_PID=$(lsof -Fp -i TCP:${TARGET_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+if [ ! -z ${TARGET_PID} ]; then
+  echo "> Kill WAS running at ${TARGET_PORT}."
+  sudo kill ${TARGET_PID}
+fi
+
+nohup java -jar \
+        -Dserver.port=${TARGET_PORT} \
+        -Dspring.profiles.active=prod \
+        /home/ubuntu/app/build/libs/* > /home/ubuntu/nohup.out 2>&1 &
+echo "> Now new WAS runs at ${TARGET_PORT}."
+sleep 10s
+exit 0
