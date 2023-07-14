bbb61aa4b1efeff1c0f65efd43d94b8ef32351ed
eujjong18 <eujjong18@naver.com>
2023-07-15 01:38:06 +0900

    FIX #9: HTTP 요청의 body가 자바 객체로 매핑되지 못하고 null이 할당되는 오류 해결 - Controller에 @RequestBody 추가

--- src/main/java/com/umc/FestieBE/domain/together/api/TogetherController.java
+++ src/main/java/com/umc/FestieBE/domain/together/api/TogetherController.java
@@ -5,6 +5,7 @@ import com.umc.FestieBE.domain.together.dto.TogetherDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,7 +15,7 @@ public class TogetherController {
     private final TogetherService togetherService;
 
     @PostMapping("/together")
-    public ResponseEntity<Void> createTogether(TogetherDTO.TogetherRequest request) {
+    public ResponseEntity<Void> createTogether(@RequestBody TogetherDTO.TogetherRequest request) {
         togetherService.createTogether(request);
         return ResponseEntity.ok().build();
     }
