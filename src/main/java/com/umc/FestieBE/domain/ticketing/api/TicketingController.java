ed34cf0c9b35ae6424b338b7c7cffd551a900566
Eunse Jang <senuej37@gmail.com>
2023-07-18 21:08:27 +0900

    FEAT #13: 티켓팅 게시글 등록

--- src/main/java/com/umc/FestieBE/domain/ticketing/api/TicketingController.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/api/TicketingController.java
@@ -1,7 +1,21 @@
 package com.umc.FestieBE.domain.ticketing.api;
+import com.umc.FestieBE.domain.ticketing.application.TicketingService;
+import com.umc.FestieBE.domain.ticketing.dto.TicketingDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
+@RequestMapping("/ticketing")
+@RequiredArgsConstructor
 @RestController
 public class TicketingController {
+    private final TicketingService ticketingService;
+
+    @PostMapping("")
+    public ResponseEntity<Void> createTicketing(@Valid @RequestBody TicketingDTO.TicketingRequest request){
+        ticketingService.createTicketing(request);
+        return ResponseEntity.ok().build();
+    }
 }
