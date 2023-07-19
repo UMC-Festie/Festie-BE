39141d90d0ac83779e03daf6e50d2fdf04096971
Eunse Jang <senuej37@gmail.com>
2023-07-19 20:17:58 +0900

    FEAT #15: 캘린더 등록 Controller 로직 구현

--- src/main/java/com/umc/FestieBE/domain/calendar/api/CalendarController.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/api/CalendarController.java
@@ -1,7 +1,25 @@
 package com.umc.FestieBE.domain.calendar.api;
 
+import com.umc.FestieBE.domain.calendar.application.CalendarService;
+import com.umc.FestieBE.domain.calendar.dto.CalendarRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
+@RequestMapping("/Calendar")
+@RequiredArgsConstructor
 @RestController
 public class CalendarController {
+    private final CalendarService calendarService;
+
+    @PostMapping("")
+    public ResponseEntity<Void> createCalendar(@Valid @RequestBody CalendarRequestDTO.CalendarRequest request){
+        calendarService.createCalendar(request);
+        return ResponseEntity.ok().build();
+    }
 }
