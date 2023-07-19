efb452ae2bbec86523102aa891d117aee851c2b9
Eunse Jang <senuej37@gmail.com>
2023-07-19 20:17:39 +0900

    FEAT #15: 캘린더 등록 service 로직 구현

--- src/main/java/com/umc/FestieBE/domain/calendar/application/CalendarService.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/application/CalendarService.java
@@ -1,7 +1,27 @@
 package com.umc.FestieBE.domain.calendar.application;
 
+import com.umc.FestieBE.domain.calendar.dao.CalendarRepository;
+import com.umc.FestieBE.domain.calendar.domain.Calendar;
+import com.umc.FestieBE.domain.calendar.dto.CalendarRequestDTO;
+import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
+import com.umc.FestieBE.domain.temporary_user.TemporaryUserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class CalendarService {
+    private final CalendarRepository calendarRepository;
+
+    // 임시 유저
+    private final TemporaryUserService temporaryUserService;
+
+    // 캘린더 등록
+    public void createCalendar(CalendarRequestDTO.CalendarRequest request){
+        TemporaryUser tempUser = temporaryUserService.createTemporaryUser();
+
+        Calendar calendar;
+        calendar = request.toEntity(tempUser);
+        calendarRepository.save(calendar);
+    }
 }
