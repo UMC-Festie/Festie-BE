d7f82bbae752c87474fb0b471a516f54f5f49743
Eunse Jang <senuej37@gmail.com>
2023-07-19 21:50:48 +0900

    FEAT #17: 캘린더 삭제 Controller 로직 구현

--- src/main/java/com/umc/FestieBE/domain/calendar/api/CalendarController.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/api/CalendarController.java
@@ -2,12 +2,11 @@ package com.umc.FestieBE.domain.calendar.api;
 
 import com.umc.FestieBE.domain.calendar.application.CalendarService;
 import com.umc.FestieBE.domain.calendar.dto.CalendarRequestDTO;
+import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
+import com.umc.FestieBE.domain.temporary_user.TemporaryUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -16,10 +15,19 @@ import javax.validation.Valid;
 @RestController
 public class CalendarController {
     private final CalendarService calendarService;
+    private final TemporaryUserService temporaryUserService;
 
+    // 캘린더 등록
     @PostMapping("")
     public ResponseEntity<Void> createCalendar(@Valid @RequestBody CalendarRequestDTO.CalendarRequest request){
         calendarService.createCalendar(request);
+        return ResponseEntity.ok().build();
+    }
+
+    // 캘린더 삭제
+    @DeleteMapping("/{calendarId}")
+    public ResponseEntity<Void> deleteCalendar(@PathVariable Long calendarId, @Valid @RequestBody TemporaryUser tempUser) {
+        calendarService.deleteCalendar(calendarId, tempUser);
         return ResponseEntity.ok().build();
     }
 }
