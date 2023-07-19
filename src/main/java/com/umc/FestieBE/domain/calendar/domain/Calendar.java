e3b4e734a4aeb28c52fc7355948223cee7647df1
Eunse Jang <senuej37@gmail.com>
2023-07-19 20:18:59 +0900

    FEAT #15: 캘린더 entity에 임시 유저(temporaryUser) 추가

--- src/main/java/com/umc/FestieBE/domain/calendar/domain/Calendar.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/domain/Calendar.java
@@ -1,16 +1,19 @@
 package com.umc.FestieBE.domain.calendar.domain;
 
 import com.umc.FestieBE.domain.BaseTimeEntity;
+import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
 import com.umc.FestieBE.domain.user.domain.User;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Calendar extends BaseTimeEntity {
     @Id
@@ -21,6 +24,11 @@ public class Calendar extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    // 임시 유저 (테스트용)
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "temporary_user_id", nullable = false)
+    private TemporaryUser temporaryUser;
 
     @Column(nullable = false)
     private String title;
