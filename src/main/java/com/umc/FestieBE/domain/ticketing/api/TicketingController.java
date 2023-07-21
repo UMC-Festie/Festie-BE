74238484b5788fffd10b891bab9a2be6123d4658
Eunse Jang <senuej37@gmail.com>
2023-07-21 20:12:23 +0900

    FEAT #19: 티켓팅 등록 Controller 수정

--- src/main/java/com/umc/FestieBE/domain/ticketing/api/TicketingController.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/api/TicketingController.java
@@ -1,6 +1,7 @@
 package com.umc.FestieBE.domain.ticketing.api;
 import com.umc.FestieBE.domain.ticketing.application.TicketingService;
-import com.umc.FestieBE.domain.ticketing.dto.TicketingDTO;
+import com.umc.FestieBE.domain.ticketing.dto.TicketingRequestDTO;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,7 +15,7 @@ public class TicketingController {
     private final TicketingService ticketingService;
 
     @PostMapping("")
-    public ResponseEntity<Void> createTicketing(@Valid @RequestBody TicketingDTO.TicketingRequest request){
+    public ResponseEntity<Void> createTicketing(@Valid @RequestBody TicketingRequestDTO.TicketingRequest request) {
         ticketingService.createTicketing(request);
         return ResponseEntity.ok().build();
     }
