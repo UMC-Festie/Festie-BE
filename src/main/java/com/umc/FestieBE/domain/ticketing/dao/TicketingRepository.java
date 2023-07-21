77d076b907a0882cf741e81dfdc87a87798fa501
Eunse Jang <senuej37@gmail.com>
2023-07-21 20:13:09 +0900

    FEAT #19: 티켓팅 Repository 수정

--- src/main/java/com/umc/FestieBE/domain/ticketing/dao/TicketingRepository.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/dao/TicketingRepository.java
@@ -4,7 +4,7 @@ import com.umc.FestieBE.domain.ticketing.domain.Ticketing;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+// @Repository
 public interface TicketingRepository extends JpaRepository<Ticketing, Long> {
 
 }
