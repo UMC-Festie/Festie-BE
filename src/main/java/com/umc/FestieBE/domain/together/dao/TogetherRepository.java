0baf59f7d94c4073656c3a4f759957d81f31abe0
eujjong18 <eujjong18@naver.com>
2023-07-21 13:35:32 +0900

    FIX #12: 같이가요 게시글 조회 수 증가 쿼리에 @Transactional 추가

--- src/main/java/com/umc/FestieBE/domain/together/dao/TogetherRepository.java
+++ src/main/java/com/umc/FestieBE/domain/together/dao/TogetherRepository.java
@@ -5,11 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface TogetherRepository extends JpaRepository<Together, Long> {
 
+    @Transactional
     @Modifying
     @Query(value = "update Together t set t.view = t.view + 1 " +
             "where t.id = :togetherId")
