0880320f3daf8b31f1e9a688752632f8c1996c7b
Eunse Jang <senuej37@gmail.com>
2023-07-22 22:13:44 +0900

    FIX #26: 캘린더 등록 Service 수정

--- src/main/java/com/umc/FestieBE/domain/calendar/application/CalendarService.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/application/CalendarService.java
@@ -3,6 +3,8 @@ package com.umc.FestieBE.domain.calendar.application;
 import com.umc.FestieBE.domain.calendar.dao.CalendarRepository;
 import com.umc.FestieBE.domain.calendar.domain.Calendar;
 import com.umc.FestieBE.domain.calendar.dto.CalendarRequestDTO;
+import com.umc.FestieBE.domain.festival.dao.FestivalRepository;
+import com.umc.FestieBE.domain.festival.domain.Festival;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUserService;
 import com.umc.FestieBE.global.exception.CustomErrorCode;
@@ -14,28 +16,29 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CalendarService {
     private final CalendarRepository calendarRepository;
+    private final FestivalRepository festivalRepository;
 
     // 임시 유저
     private final TemporaryUserService temporaryUserService;
 
-    // 캘린더 등록
-    public void createCalendar(CalendarRequestDTO.CalendarRequest request){
+    // [캘린더 등록]
+    // 1. 연동 O -> 제목만 연동하고, 나머지는 사용자가 직접 입력 (= 날짜, 시간)
+    // 2. 연동 X -> 사용자가 제목, 날짜, 시간 전부 직접 입력
+    public void createCalendar(CalendarRequestDTO request) {
         TemporaryUser tempUser = temporaryUserService.createTemporaryUser();
 
+        Festival festival;
         Calendar calendar;
-        calendar = request.toEntity(tempUser);
-        calendarRepository.save(calendar);
-    }
 
-    // 캘린더 삭제
-    public void deleteCalendar(Long calendarId, TemporaryUser tempUser) {
-        Calendar calendar = calendarRepository.findById(calendarId)
-                .orElseThrow(() -> new CustomException(CustomErrorCode.CALENDAR_NOT_FOUND));
-
-        if(!calendar.getTemporaryUser().equals(tempUser)) {
-            throw new CustomException(CustomErrorCode.CALENDAR_USER_MISMATCH);
+        // 공연/축제 정보 연동 시 DB 에서 확인
+        if (request.getFestivalId() != null) {// 1. 연동 O
+            festival = festivalRepository.findById(request.getFestivalId())
+                    .orElseThrow(() -> (new CustomException(CustomErrorCode.FESTIVAL_NOT_FOUND)));
+            calendar = request.toEntity(tempUser, festival);
+            calendarRepository.save(calendar);
+        } else { // 2. 연동 X
+            calendar = request.toEntity(tempUser);
+            calendarRepository.save(calendar);
         }
-
-        calendarRepository.delete(calendar);
     }
 }
