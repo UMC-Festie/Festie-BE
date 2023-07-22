ee0e04daba4aedc6abccc13b7c614881f2afbbe7
Eunse Jang <senuej37@gmail.com>
2023-07-22 13:43:54 +0900

    FEAT #19: 티켓팅 등록 DTO 수정 (연동 여부에 따라 toEntity 분리)

--- src/main/java/com/umc/FestieBE/domain/ticketing/dto/TicketingRequestDTO.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/dto/TicketingRequestDTO.java
@@ -5,55 +5,63 @@ import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
 import com.umc.FestieBE.domain.ticketing.domain.Ticketing;
 import com.umc.FestieBE.global.type.FestivalType;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import java.time.LocalTime;
 
+@Getter
+@NoArgsConstructor
 public class TicketingRequestDTO {
-    @Getter
-    public static class TicketingRequest {
-        @NotBlank(message = "티켓팅 제목은 필수 입력 값입니다.")
-        private String title;
-        @NotBlank(message = "티켓팅 작성 내용은 필수 입력 값입니다.")
-        private String content;
+    @NotBlank(message = "티켓팅 제목은 필수 입력 값입니다.")
+    private String title;
+    @NotBlank(message = "티켓팅 작성 내용은 필수 입력 값입니다.")
+    private String content;
 
-        private Long festivalId;
-        private String festivalTitle;
-        private String thumbnailUrl;
+    private Long festivalId;
+    private String festivalTitle;
+    private String thumbnailUrl;
 
-        @Min(value = 0, message = "공연/축제 유형은 0(공연) 또는 1(축제)만 가능합니다.")
-        @Max(value = 1, message = "공연/축제 유형은 0(공연) 또는 1(축제)만 가능합니다.")
-        private Integer festivalType;
-        private Integer festivalCategory;
-        private LocalDate festivalDate;
+    @Min(value = 0, message = "공연/축제 유형은 0(공연) 또는 1(축제)만 가능합니다.")
+    @Max(value = 1, message = "공연/축제 유형은 0(공연) 또는 1(축제)만 가능합니다.")
+    private Integer festivalType;
 
-        // 공통으로 들어가는 속성 (필수 값)
-        private Ticketing.TicketingBuilder buildCommonProperties() {
-            return Ticketing.builder()
-                    .title(title)
-                    .content(content)
-                    .view(0L);
-        }
+    private Integer festivalCategory;
+    private String festivalDate;
 
-        // 축제,공연 연동 X
-        public Ticketing toEntity(TemporaryUser tempUser) {
-            return buildCommonProperties()
-                    .build();
-        }
 
-        // 축제,공연 연동 O
-        public Ticketing toEntity(TemporaryUser tempUser, Festival festival) {
-            return buildCommonProperties()
-                    .temporaryUser(tempUser)
-                    .thumbnailUrl(thumbnailUrl)
-                    //type(festivalType)
-                    // 카테고리
-                    .festivalDate(festivalDate)
-                    .festivalTitle(festivalTitle)
-                    .build();
-        }
+    // 1. 축제, 공연 연동 O (festivalId != null인 경우)
+    public Ticketing toEntity(TemporaryUser tempUser,
+                              Festival festival) {
+        return Ticketing.builder()
+                .temporaryUser(tempUser)
+                .view(0L)
+                .title(title)
+                .content(content)
+                .festivalId(festivalId)
+                .festivalTitle(festival.getFestivalTitle())
+                .festivalDate(LocalDate.parse(festivalDate))
+                .type(festival.getType())
+                .category(festival.getCategory())
+                .thumbnailUrl(festival.getThumbnailUrl())
+                .build();
+    }
+
+    // 2. 축제, 공연 연동 X (festivalId == null인 경우)
+    public Ticketing toEntity(TemporaryUser tempUser, FestivalType festivalType) {
+        return Ticketing.builder()
+                .temporaryUser(tempUser)
+                .view(0L)
+                .title(title)
+                .content(content)
+                .festivalId(festivalId)
+                .festivalTitle(festivalTitle)
+                .festivalDate(LocalDate.parse(festivalDate))
+                .type(festivalType)
+                .category(festivalCategory)
+                .thumbnailUrl(thumbnailUrl)
+                .build();
     }
 }
