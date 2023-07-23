2b21e36ab3c5dfecf16b0a6e9c08b94c3160a72a
Eunse Jang <senuej37@gmail.com>
2023-07-23 18:03:04 +0900

    FIX #36: 새로운 공연/축제 Service 수정

--- src/main/java/com/umc/FestieBE/domain/festival/application/FestivalService.java
+++ src/main/java/com/umc/FestieBE/domain/festival/application/FestivalService.java
@@ -5,6 +5,8 @@ import com.umc.FestieBE.domain.festival.domain.Festival;
 import com.umc.FestieBE.domain.festival.dto.FestivalRequestDTO;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUserService;
+import com.umc.FestieBE.global.type.FestivalType;
+import com.umc.FestieBE.global.type.RegionType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,11 +18,14 @@ public class FestivalService {
     // 임시 유저
     private final TemporaryUserService temporaryUserService;
 
-    // 새로운 공연,축제 등록
-    public void createFestival(FestivalRequestDTO.FestivalRequest request){
+    // [새로운 공연,축제 등록]
+    public void createFestival(FestivalRequestDTO request){
         TemporaryUser tempUser = temporaryUserService.createTemporaryUser();
-        Festival festival = request.toEntity(tempUser);
 
+        FestivalType festivalType = FestivalType.findFestivalType(request.getFestivalType());
+        RegionType regionType = RegionType.findRegionType(request.getFestivalRegion());
+
+        Festival festival = request.toEntity(tempUser, festivalType, regionType);
         festivalRepository.save(festival);
     }
 }
