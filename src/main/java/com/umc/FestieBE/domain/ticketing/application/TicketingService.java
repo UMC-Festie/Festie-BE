d5233afea5751145213250d29319c89709821bc3
Eunse Jang <senuej37@gmail.com>
2023-07-23 11:39:30 +0900

    FEAT #33: 티켓팅 수정 API Service 구현

--- src/main/java/com/umc/FestieBE/domain/ticketing/application/TicketingService.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/application/TicketingService.java
@@ -48,4 +48,43 @@ public class TicketingService {
                 .orElseThrow(() -> new CustomException(CustomErrorCode.TICKETING_NOT_FOUND));
         ticketingRepository.delete(ticketing);
     }
+
+    // [티켓팅 수정]
+    public void updateTicketing(Long ticketingId, TicketingRequestDTO request) {
+        TemporaryUser tempUser = temporaryUserService.createTemporaryUser();
+
+        Ticketing ticketing = ticketingRepository.findById(ticketingId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.TICKETING_NOT_FOUND));
+
+        if (request.getFestivalId() != null) { // 1. 새롭게 연동할 경우
+            Festival festival = festivalRepository.findById(request.getFestivalId())
+                    .orElseThrow(() -> new CustomException(CustomErrorCode.FESTIVAL_NOT_FOUND));
+
+            ticketing.clearFestivalInfo(); // 기존에 연동된 내용 삭제
+
+            ticketing.updateTicketing( // 새롭게 다시 작성한 글 업데이트
+                    request.getFestivalId(),
+                    festival.getTitle(),
+                    festival.getThumbnailUrl(),
+                    festival.getCategory(),
+                    request.getFestivalDate(),
+                    request.getTitle(),
+                    request.getContent()
+            );
+        }
+        else { // 2. 연동 안할 경우 -> 사용자가 다시 재작성
+            ticketing.clearFestivalInfo(); // 기존에 연동된 내용 삭제
+
+            ticketing.updateTicketing( // 새롭게 다시 작성한 글 업데이트
+                    request.getFestivalId(),
+                    request.getFestivalTitle(),
+                    request.getThumbnailUrl(),
+                    request.getFestivalCategory(),
+                    request.getFestivalDate(),
+                    request.getTitle(),
+                    request.getContent()
+            );
+        }
+        ticketingRepository.save(ticketing);
+    }
 }
