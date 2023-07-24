cf72a0efce5c7ea1ba711665501e277954bbb93a
eujjong18 <eujjong18@naver.com>
2023-07-25 02:31:17 +0900

    FEAT #45: 게시글 좋아요/싫어요 Request DTO에 validation 추가 - 좋아요/싫어요 상태를 나타내는 status에 @NotNull 추가

--- src/main/java/com/umc/FestieBE/domain/like_or_dislike/dto/LikeOrDislikeRequestDTO.java
+++ src/main/java/com/umc/FestieBE/domain/like_or_dislike/dto/LikeOrDislikeRequestDTO.java
@@ -8,10 +8,14 @@ import com.umc.FestieBE.domain.ticketing.domain.Ticketing;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class LikeOrDislikeRequestDTO {
+    @NotNull(message = "좋아요 또는 싫어요 상태는 필수 입력값입니다.")
     private Integer status; // 좋아요(1)/싫어요(0) 상태
+
     private Long festivalId;
     private Long ticketingId;
     private Long reviewId;
