ac425bc4e27a3063a19875295e7604ac8fa164d1
Eunse Jang <senuej37@gmail.com>
2023-07-24 14:06:17 +0900

    FEAT #40: 새로운 공연/축제 삭제 Service 로직 구현 (isDeleted 처리)

--- src/main/java/com/umc/FestieBE/domain/festival/application/FestivalService.java
+++ src/main/java/com/umc/FestieBE/domain/festival/application/FestivalService.java
@@ -5,6 +5,9 @@ import com.umc.FestieBE.domain.festival.domain.Festival;
 import com.umc.FestieBE.domain.festival.dto.FestivalRequestDTO;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUser;
 import com.umc.FestieBE.domain.temporary_user.TemporaryUserService;
+import com.umc.FestieBE.domain.ticketing.domain.Ticketing;
+import com.umc.FestieBE.global.exception.CustomErrorCode;
+import com.umc.FestieBE.global.exception.CustomException;
 import com.umc.FestieBE.global.type.FestivalType;
 import com.umc.FestieBE.global.type.RegionType;
 import lombok.RequiredArgsConstructor;
@@ -14,7 +17,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class FestivalService {
     private final FestivalRepository festivalRepository;
-
     // 임시 유저
     private final TemporaryUserService temporaryUserService;
 
@@ -24,8 +26,38 @@ public class FestivalService {
 
         FestivalType festivalType = FestivalType.findFestivalType(request.getFestivalType());
         RegionType regionType = RegionType.findRegionType(request.getFestivalRegion());
+        Boolean isDeleted = false;
 
-        Festival festival = request.toEntity(tempUser, festivalType, regionType);
+        Festival festival = request.toEntity(tempUser, festivalType, regionType, isDeleted);
+        festivalRepository.save(festival);
+    }
+
+    // [새로운 공연, 축제 삭제]
+    // * 새로운 공연, 축제 삭제 시 해당 데이터가 진짜 삭제 되면 안됨!
+    // : 데이터 삭제 안하고 isDeleted값이 true가 되도록 함
+    public void deleteFestival(Long festivalId, FestivalRequestDTO request) {
+        Festival festival = festivalRepository.findById(festivalId)
+                .orElseThrow(() -> new CustomException(CustomErrorCode.FESTIVAL_NOT_FOUND));
+
+        Boolean isDeleted = true; // isDeleted로 삭제 여부 표기
+        festival.updateIsDeleted(festival.getId(),
+                request.getFestivalTitle(),
+                request.getFestivalType(),
+                request.getThumbnailUrl(),
+                request.getFestivalCategory(),
+                request.getFestivalRegion(),
+                request.getFestivalLocation(),
+                request.getFestivalStartDate(),
+                request.getFestivalEndDate(),
+                request.getFestivalStartTime(),
+                request.getPostTitle(),
+                request.getContent(),
+                request.getFestivalAdminsName(),
+                request.getFestivalAdminsPhone(),
+                request.getFestivalAdminsSiteAddress(),
+                isDeleted
+                );
+
         festivalRepository.save(festival);
     }
 }
