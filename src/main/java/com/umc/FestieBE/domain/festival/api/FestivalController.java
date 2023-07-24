586484de345f81a88d39342f6701cf48919e6e6d
Eunse Jang <senuej37@gmail.com>
2023-07-24 14:02:29 +0900

    FEAT #40: 새로운 공연/축제 삭제 API Controller 구현

--- src/main/java/com/umc/FestieBE/domain/festival/api/FestivalController.java
+++ src/main/java/com/umc/FestieBE/domain/festival/api/FestivalController.java
@@ -4,10 +4,7 @@ import com.umc.FestieBE.domain.festival.application.FestivalService;
 import com.umc.FestieBE.domain.festival.dto.FestivalRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,9 +14,18 @@ import javax.validation.Valid;
 public class FestivalController {
     private final FestivalService festivalService;
 
+    // [새로운 공연/축제 등록]
     @PostMapping("")
     public ResponseEntity<Void> createFestival(@Valid @RequestBody FestivalRequestDTO request) {
         festivalService.createFestival(request);
+        return ResponseEntity.ok().build();
+    }
+
+    // [새로운 공연/축제 삭제] -> 기능상 내용 삭제 X, 수정되어야 해서 PutMapping 사용
+    @PutMapping("/{festivalId}")
+    public ResponseEntity<Void> deleteFestival(@PathVariable Long festivalId,
+                                               @Valid @RequestBody FestivalRequestDTO request) {
+        festivalService.deleteFestival(festivalId, request);
         return ResponseEntity.ok().build();
     }
 }
