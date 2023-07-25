036a0aba515c04571200dd79f45943ef51f3a417
Eunse Jang <senuej37@gmail.com>
2023-07-25 15:44:37 +0900

    FEAT #44: 캘린더 조회 쿼리 주석 처리 (임시 유저)

--- src/main/java/com/umc/FestieBE/domain/calendar/dao/CalendarRepository.java
+++ src/main/java/com/umc/FestieBE/domain/calendar/dao/CalendarRepository.java
@@ -1,11 +1,25 @@
 package com.umc.FestieBE.domain.calendar.dao;
 
 import com.umc.FestieBE.domain.calendar.domain.Calendar;
+import com.umc.FestieBE.domain.together.domain.Together;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 
 @Repository
 public interface CalendarRepository extends JpaRepository<Calendar, Long> {
 
+    /** 유저 권환 필요 X */
+    /*
+    @Query("SELECT c FROM Calendar c " +
+            "JOIN FETCH c.temporaryUser u " + // 임시 유저
+            "WHERE c.id = :calendarId")
+    Optional<Calendar> findByIdWithUser(@Param("calendarId") Long calendarId);
+     */
 }
