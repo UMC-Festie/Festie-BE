341662bb2e09253070d6d2d9cf2bf5c24a17c2fa
Eunse Jang <senuej37@gmail.com>
2023-07-26 13:21:39 +0900

    FEAT #47: 티켓팅 게시글 쿼리 추가 (조회수, 임시유저 정보)

--- src/main/java/com/umc/FestieBE/domain/ticketing/dao/TicketingRepository.java
+++ src/main/java/com/umc/FestieBE/domain/ticketing/dao/TicketingRepository.java
@@ -1,10 +1,30 @@
 package com.umc.FestieBE.domain.ticketing.dao;
 
 import com.umc.FestieBE.domain.ticketing.domain.Ticketing;
+import com.umc.FestieBE.domain.together.domain.Together;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 // @Repository
 public interface TicketingRepository extends JpaRepository<Ticketing, Long> {
+
+    // 조회수
+    @Transactional
+    @Modifying
+    @Query("UPDATE Ticketing t SET t.view = t.view + 1 " +
+            "WHERE t.id = :ticketingId")
+    void updateView(@Param("ticketingId") Long ticketingId);
+
+    // 임시 유저
+    @Query("SELECT t FROM Ticketing t " +
+            "JOIN FETCH t.temporaryUser u " + //임시 유저
+            "WHERE t.id = :ticketingId")
+    Optional<Ticketing> findByIdWithUser(@Param("ticketingId") Long ticketingId);
 
 }
