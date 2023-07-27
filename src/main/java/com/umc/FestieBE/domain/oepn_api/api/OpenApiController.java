c147fc4aa01c51f58e81d432a8ad88c883937c2f
gurino <2021111786@dgu.ac.kr>
2023-07-27 17:14:36 +0900

    FEAT #11: 코드 정리

--- src/main/java/com/umc/FestieBE/domain/oepn_api/api/OpenApiController.java
+++ src/main/java/com/umc/FestieBE/domain/oepn_api/api/OpenApiController.java
@@ -11,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
-
 @RestController
 public class OpenApiController {
     private final OpenApiService openApiService;
