5601f28e553be8c1c32f923e53f4c664c9710861
Eunse Jang <senuej37@gmail.com>
2023-07-30 13:56:38 +0900

    FEAT #57: 새로운 축제/공연 목록조회 ResponseDTO 추가 구현

--- src/main/java/com/umc/FestieBE/domain/festival/dto/FestivalPaginationResponseDTO.java
+++ src/main/java/com/umc/FestieBE/domain/festival/dto/FestivalPaginationResponseDTO.java
@@ -1,6 +1,7 @@
 package com.umc.FestieBE.domain.festival.dto;
 
 import com.umc.FestieBE.domain.festival.domain.Festival;
+import com.umc.FestieBE.global.type.SortedType;
 import lombok.Getter;
 
 import java.time.format.DateTimeFormatter;
@@ -13,11 +14,11 @@ public class FestivalPaginationResponseDTO {
 
     private Integer TotalCount; // 검색결과 건수
 
-    private String festivalDate; // 무한스크롤에서 제공할 공연 기간 (ex. 2023.5.30 - 2023.8.20)
+    private String festivalDate; // 목록조회에서 표시될 공연 기간 (ex. 2023.5.30 - 2023.8.20)
     private String thumbnailUrl;
 
-    // TODO 삭제 처리된건 무한스크롤에 반영되면 안됨
     private Boolean isDeleted;
+
 
     public FestivalPaginationResponseDTO(Festival festival, String dDay, Integer totalCount) {
         DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.M.dd");
