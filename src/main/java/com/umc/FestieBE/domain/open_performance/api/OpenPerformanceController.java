531ddf91cbe91ce9773f43bd3135dab3d6426745
gurino <2021111786@dgu.ac.kr>
2023-08-03 01:29:19 +0900

    FEAT #86 : json형태로 호출 성공

--- src/main/java/com/umc/FestieBE/domain/open_performance/api/OpenPerformanceController.java
+++ src/main/java/com/umc/FestieBE/domain/open_performance/api/OpenPerformanceController.java
@@ -1,7 +1,45 @@
 package com.umc.FestieBE.domain.open_performance.api;
 
+import com.umc.FestieBE.domain.open_performance.application.OpenPerformanceService;
+import com.umc.FestieBE.domain.open_performance.dto.OpenPerformanceDTO;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URL;
 
 @RestController
 public class OpenPerformanceController {
+    private final OpenPerformanceService openPerformanceService;
+
+    public OpenPerformanceController(OpenPerformanceService openPerformanceService){this.openPerformanceService = openPerformanceService;}
+
+
+    @GetMapping("/open/performance-list")
+    public ResponseEntity<OpenPerformanceDTO[]> getPerform(
+            @RequestParam(value = "category",required = false) Integer category,
+            @RequestParam(value = "region",required = false) String region,
+            @RequestParam(value = "period",required = false) Integer period,
+            @RequestParam(value = "sort",required = false) Integer sort) throws ParseException {
+        //서비스를 통해 openapi 호출 및 데이터 반환
+        OpenPerformanceDTO[] jsonResult = openPerformanceService.getPerform(category, region, period, sort);
+        if (jsonResult == null) {
+            // 데이터를 가져오지 못했을 경우에 대한 예외 처리 (이 부분 나중에 변경)
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return new ResponseEntity<>(jsonResult, headers, HttpStatus.OK);
+    }
+
+
 }
+
+
+
--- src/main/java/com/umc/FestieBE/domain/open_performance/application/OpenPerformanceService.java
+++ src/main/java/com/umc/FestieBE/domain/open_performance/application/OpenPerformanceService.java
@@ -1,7 +1,61 @@
 package com.umc.FestieBE.domain.open_performance.application;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.umc.FestieBE.domain.open_performance.dto.OpenPerformanceDTO;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Collections;
 
 @Service
 public class OpenPerformanceService {
+    @Value("${openapi.FIXED_API_KEY}")
+    private String FIXED_API_KEY;
+    //OpenAPI 호출
+    RestTemplate restTemplate = new RestTemplate();
+    public OpenPerformanceDTO[] getPerform(Integer category, String region, Integer period, Integer sort) throws ParseException {
+        String apiUrl = "http://www.kopis.or.kr/openApi/restful/pblprfr";
+
+        UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(apiUrl)
+                .queryParam("service", FIXED_API_KEY) // 서비스키를 고정값으로 추가
+                .queryParam("stdate", "20230701")
+                .queryParam("eddate", "20230830")
+                .queryParam("cpage", "1")
+                .queryParam("rows", "8");
+
+        //Json 형식의 응답을 기대하도록 헤더 설정
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_XML);
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_XML));
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        //XML변환을 위한 HttpMessageConverter 등록
+        restTemplate.getMessageConverters().add(
+                new MappingJackson2XmlHttpMessageConverter(new XmlMapper()));
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                builder.toUriString(), HttpMethod.GET, entity, String.class
+        );
+
+        //xml 매핑하기
+        XmlMapper xmlMapper = new XmlMapper();
+        OpenPerformanceDTO[] dtos;
+        try {
+            dtos = xmlMapper.readValue(response.getBody(), OpenPerformanceDTO[].class);
+        }catch (JsonProcessingException e){
+            e.printStackTrace();
+            return null;
+        }
+
+        return dtos;
+
+    }
+
+
 }
