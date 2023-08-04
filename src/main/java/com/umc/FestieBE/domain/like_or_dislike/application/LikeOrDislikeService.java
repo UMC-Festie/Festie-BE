3d89a5e1ddeb653820a5e746ee19a4c22b66c539
Eunse Jang <senuej37@gmail.com>
2023-08-04 20:42:18 +0900

    REFACTOR #84: LikeOrDislikeService 수정한 코드 삭제

--- src/main/java/com/umc/FestieBE/domain/like_or_dislike/application/LikeOrDislikeService.java
+++ src/main/java/com/umc/FestieBE/domain/like_or_dislike/application/LikeOrDislikeService.java
@@ -64,7 +64,6 @@ public class LikeOrDislikeService {
         // 좋아요/싫어요 내역 조회
         Long findLikes = likeOrDislikeRepository.findByTargetIdAndUserId(tempUser.getId(),festivalId, ticketingId, reviewId);
 
-        // TODO 질문..
         if(findLikes != 0){
             throw new CustomException(CustomErrorCode.LIKES_ALREADY_EXISTS);
         }
