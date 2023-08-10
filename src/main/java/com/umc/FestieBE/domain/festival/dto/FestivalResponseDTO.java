1fd5e718e9fa2aec7aab27a558155426311a4ea2
Eunse Jang <senuej37@gmail.com>
2023-08-10 15:54:39 +0900

    REFACTOR #119: 새로운 공연/축제 목록조회 ResponseDTO 데이터 수정

--- src/main/java/com/umc/FestieBE/domain/festival/dto/FestivalResponseDTO.java
+++ src/main/java/com/umc/FestieBE/domain/festival/dto/FestivalResponseDTO.java
@@ -11,65 +11,115 @@ import java.util.List;
 
 @Getter
 public class FestivalResponseDTO {
-    // TODO 디데이 쿼리 추가
-    private String dDay;
-    private String festivalTitle;
-    private String postTitle;
-    private String content;
-    private String location;
-    private RegionType region;
-    private String startDate;
-    private String endDate;
-    private String startTime;
 
-    private String type;
+    // 공연, 축제 상세조회
+    @Getter
+    public static class FestivalDetailResponse {
+        private String dDay;
+        private String festivalTitle;
+        private String postTitle;
+        private String content;
+        private String location;
+        private RegionType region;
+        private String startDate;
+        private String endDate;
+        private String startTime;
 
-    private String reservationLink;
-    private String thumbnailUrl;
-    private Long view;
+        private String type;
 
-    private String adminsName;
-    private String adminsPhone;
-    private String adminsSiteAddress;
+        private String reservationLink;
+        private String thumbnailUrl;
+        private Long view;
 
-    private Boolean isWriter;
-    private Boolean isDeleted;
+        private String adminsName;
+        private String adminsPhone;
+        private String adminsSiteAddress;
 
-    private Long like;
-    private Long dislike;
+        private Boolean isWriter;
+        private Boolean isDeleted;
 
-    private List<String> imagesUrl;
+        private Long like;
+        private Long dislike;
 
+        private List<String> imagesUrl;
 
-    public FestivalResponseDTO (Festival festival, Boolean isWriter, String dDay, Long like, Long dislike){
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        String startDate = festival.getStartDate().format(dateFormatter);
-        String endDate = festival.getEndDate().format(dateFormatter);
+        public FestivalDetailResponse (Festival festival, Boolean isWriter, String dDay, Long like, Long dislike){
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
+            String startDate = festival.getStartDate().format(dateFormatter);
+            String endDate = festival.getEndDate().format(dateFormatter);
 
-        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm ~");
-        String startTime = festival.getStartTime().format(timeFormatter);
+            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm ~");
+            String startTime = festival.getStartTime().format(timeFormatter);
 
-        this.type = festival.getType().getType();
-        this.like = like;
-        this.dislike = dislike;
-        this.dDay = dDay;
-        this.festivalTitle = festival.getFestivalTitle();
-        this.postTitle = festival.getTitle();
-        this.content = festival.getContent();
-        this.reservationLink = festival.getReservationLink();
-        this.location = festival.getLocation();
-        this.region = festival.getRegion();
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.startTime = startTime;
-        this.thumbnailUrl = festival.getThumbnailUrl();
-        this.view = festival.getView();
-        this.adminsName = festival.getAdminsName();
-        this.adminsPhone = festival.getAdminsPhone();
-        this.adminsSiteAddress = festival.getAdminsSiteAddress();
-        this.isDeleted = festival.getIsDeleted();
-        this.imagesUrl = festival.getImagesUrl();
+            this.type = festival.getType().getType();
+            this.like = like;
+            this.dislike = dislike;
+            this.dDay = dDay;
+            this.festivalTitle = festival.getFestivalTitle();
+            this.postTitle = festival.getTitle();
+            this.content = festival.getContent();
+            this.reservationLink = festival.getReservationLink();
+            this.location = festival.getLocation();
+            this.region = festival.getRegion();
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.startTime = startTime;
+            this.thumbnailUrl = festival.getThumbnailUrl();
+            this.view = festival.getView();
+            this.adminsName = festival.getAdminsName();
+            this.adminsPhone = festival.getAdminsPhone();
+            this.adminsSiteAddress = festival.getAdminsSiteAddress();
+            this.isDeleted = festival.getIsDeleted();
+            this.imagesUrl = festival.getImagesUrl();
 
-        this.isWriter = isWriter;
+            this.isWriter = isWriter;
+        }
+    }
+
+    // 공연, 축제 목록조회
+    @Getter
+    public static class FestivalPaginationResponse {
+        private String dDay;
+        private String festivalTitle;
+        private String location;
+        private String festivalDate; // 목록조회에서 표시될 공연 기간 (ex. 2023.5.30 - 2023.8.20)
+        private String thumbnailUrl;
+        private Boolean isDeleted;
+
+        public FestivalPaginationResponse (Festival festival, String dDay) {
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy.M.dd");
+            String startDate = festival.getStartDate().format(dateFormatter);
+            String endDate = festival.getEndDate().format(dateFormatter);
+            String festivalDate = startDate + " - " + endDate;
+
+            this.thumbnailUrl = festival.getThumbnailUrl();
+            this.festivalTitle = festival.getFestivalTitle();
+            this.location = festival.getLocation();
+            this.festivalDate = festivalDate;
+            this.isDeleted = festival.getIsDeleted();
+            this.dDay = dDay;
+        }
+    }
+
+    // 공연, 축제 무한스크롤
+    @Getter
+    public static class FestivalListResponse {
+        private Long totalCount;
+        private Integer pageNum;
+        private Boolean hasNext;
+        private Boolean hasPrevious;
+        private List<FestivalPaginationResponse> data;
+
+        public FestivalListResponse(List<FestivalPaginationResponse> data,
+                                    Long totalCount,
+                                    Integer pageNum,
+                                    Boolean hasNext,
+                                    Boolean hasPrevious) {
+            this.totalCount = totalCount;
+            this.pageNum = pageNum;
+            this.hasNext = hasNext;
+            this.hasPrevious = hasPrevious;
+            this.data = data;
+        }
     }
 }
