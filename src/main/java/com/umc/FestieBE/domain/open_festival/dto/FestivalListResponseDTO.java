5a95baeb55977f5d8185332f177eece52effbfe9
eujjong18 <eujjong18@naver.com>
2023-08-20 17:45:53 +0900

    FEAT #147: 같이가요 상세조회 - 공연/축제 연동 정보 관련 DTO 수정 - FestivalLinkResponseDTO 중 정보보기 region 값 설정

--- src/main/java/com/umc/FestieBE/domain/open_festival/dto/FestivalListResponseDTO.java
+++ src/main/java/com/umc/FestieBE/domain/open_festival/dto/FestivalListResponseDTO.java
@@ -21,21 +21,6 @@ public class FestivalListResponseDTO {
         private String startDate;
         private String endDate;
 
-        /*
-        public FestivalHomeListResponse(Festival festival, Integer status, Long dDay){
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.M.d");
-
-            this.festivalId = festival.getId();
-            this.thumbnailUrl = festival.getThumbnailUrl();
-            this.status = status;
-            this.dDay = dDay;
-            this.title = festival.getFestivalTitle();
-            this.location = festival.getLocation();
-            this.startDate = festival.getStartDate().format(formatter);
-            this.endDate = festival.getEndDate().format(formatter);
-        }
-        */
-
         public FestivalHomeListResponse(OpenPerformance op, Integer status, Long dDay){
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
 
